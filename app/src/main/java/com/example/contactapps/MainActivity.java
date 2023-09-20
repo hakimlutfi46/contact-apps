851a9cb4d07572c5238bfb78c41c569a617a8233
hakimlutfi46 <lutfihakim1606@gmail.com>
2023-09-20 20:17:57 +0700

    add fragment code and link to main

--- app/src/main/java/com/example/contactapps/MainActivity.java
+++ app/src/main/java/com/example/contactapps/MainActivity.java
@@ -1,17 +1,43 @@
 package com.example.contactapps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.contactapps.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFargment());
 
+        binding.navBar.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.homeId){
+                replaceFragment(new HomeFargment());
+            } else if(item.getItemId() == R.id.contactId){
+                replaceFragment(new ContactFragment());
+            } else if(item.getItemId() == R.id.profileId){
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrameLayout, fragment);
+        fragmentTransaction.commit();
 
     }
 }
